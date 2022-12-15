217e292700178d5baec4c14ec8ef2d178ba701bb
Jacob Lapenna <jlapenna@usbr.gov>
2022-12-15 14:16:41 -0700

    Iterative commit.

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ from flask_socketio import SocketIO
 
 class Data:
 
-    _socketio = SocketIO(message_queue='redis://')
+    socketio = SocketIO(message_queue='redis://')
 
     def __init__(self):
         pass
@@ -14,13 +14,13 @@ class Data:
         
         while True:
             value = randrange(0, 1000, 1) / 100
-            _socketio.emit("new_data", data={"value" :  value})
+            self.socketio.emit("new_data", data={"value" :  value})
     
     def run(self):
 
         process = Process(target=self._stream, name="data_stream")
         process.start()
     
-    @_socketio.on("connect")
+    @socketio.on("connect")
     def connect():
         print("Connection to data object!")
