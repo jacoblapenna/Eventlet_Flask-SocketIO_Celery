5c6364a37bd7e3a910bf4210e8f4992671eeee67
Jacob Lapenna <jlapenna@usbr.gov>
2022-12-19 14:51:18 -0700

    Iterative commit.

--- example_app/__init__.py
+++ example_app/__init__.py
@@ -9,9 +9,6 @@ from flask import Flask
 from flask_socketio import SocketIO
 from celery import Celery
 
-from . import views
-from . import events
-
 if redis.Redis().ping():
     pass
 else:
@@ -34,5 +31,8 @@ socketio = SocketIO(app, message_queue=app.config["MESSAGE_BROKER"])
 
 cel = Celery(__name__, broker=app.config["MESSAGE_BROKER"], include=["example_app.tasks"])
 cel.conf.update(app.config)
+
+from . import views
+from . import events
 
 socketio.run(app, host=ip, port=port, use_reloader=False, debug=True)
