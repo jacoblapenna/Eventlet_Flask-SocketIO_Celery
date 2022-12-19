70fb47744892bb3c1c8ca89793565694ebb38ac8
Jacob Lapenna <jlapenna@usbr.gov>
2022-12-19 11:49:03 -0700

    Iterative commit.

--- example_app/celery.py
+++ example_app/celery.py
@@ -1,7 +1,7 @@
 from celery import Celery
 from example_app import app
 
-cel = Celery("example_app", broker=app["MESSAGE_BROKER"], include=["example_app.tasks"])
+cel = Celery("example_app", broker=app.config["MESSAGE_BROKER"], include=["example_app.tasks"])
 
 if __name__ == "__main__":
     cel.start()
