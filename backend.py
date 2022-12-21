d5544a8d4e8cab8985fc55ffdfd78a591343b275
Jacob Lapenna <jlapenna@usbr.gov>
2022-12-21 13:38:27 -0700

    Iterative commit.

--- backend.py
+++ backend.py
@@ -1,36 +1,20 @@
 
-import eventlet
-eventlet.monkey_patch()
-
-from random import randrange
-
-import redis
+# import redis
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO
-
 from celery import Celery
 
 
-app = Flask(__name__)
 message_broker = "redis://localhost:6379/0"
-app.config.update(CELERY_CONFIG={'broker_url': message_broker, 'result_backend': message_broker})
 
-socketio = SocketIO(app, message_queue=message_broker)
+app = Flask(__name__)
 
-cel = Celery(app.import_name)
-cel.conf.update(app.config["CELERY_CONFIG"])
+cel = Celery(app.import_name, broker=message_broker, backend=message_broker)
 
-# serve page
 @app.route('/')
 def index():
-    return render_template("index.html")
-
-@socketio.on("start_data_stream")
-def start_data_stream():
-
-    print("Starting data stream...")
-    result = stream_data.delay(100, 100)
+    
+    result = add.delay(100, 100)
     s = result.status
     last_s = None
 
@@ -42,8 +26,26 @@ def start_data_stream():
     
     print(s, result.result)
 
+    return render_template("index.html")
+
+# @socketio.on("start_data_stream")
+# def start_data_stream():
+
+#     print("Starting data stream...")
+#     result = add.delay(100, 100)
+#     s = result.status
+#     last_s = None
+
+#     while s != "FAILURE" or s != "SUCCESS":
+#         if s != last_s:
+#             print(s)
+#         last_s = s
+#         s = result.status
+    
+#     print(s, result.result)
+
 @cel.task()
-def stream_data(a, b):
+def add(a, b):
     return a + b
     # data_socketio = SocketIO(message_queue=message_broker)
     # i = 1
@@ -56,14 +58,14 @@ def stream_data(a, b):
     # return i, value
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    if redis.Redis().ping():
-        pass
-    else:
-        raise Exception("You need redis: https://redis.io/docs/getting-started/installation/. Check that redis-server.service is running!")
+#     if redis.Redis().ping():
+#         pass
+#     else:
+#         raise Exception("You need redis: https://redis.io/docs/getting-started/installation/. Check that redis-server.service is running!")
 
-    ip = "192.168.1.8" # insert LAN address here
-    port = 8080
+#     ip = "192.168.1.8" # insert LAN address here
+#     port = 8080
 
-    socketio.run(app, host=ip, port=port, use_reloader=False, debug=True)
+#     socketio.run(app, host=ip, port=port, use_reloader=False, debug=True)
