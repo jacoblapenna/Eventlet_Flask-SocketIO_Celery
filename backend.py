09f0fb9d06c835f6cf6c6b8dee593a7c2841702c
Jacob Lapenna <jlapenna@usbr.gov>
2022-12-21 13:51:45 -0700

    Iterative commit.

--- backend.py
+++ backend.py
@@ -2,18 +2,25 @@
 # import redis
 
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 from celery import Celery
 
 
 message_broker = "redis://localhost:6379/0"
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 cel = Celery(app.import_name, broker=message_broker, backend=message_broker)
 
 @app.route('/')
 def index():
-    
+    return render_template("index.html")
+
+@socketio.on("start_data_stream")
+def start_data_stream():
+
+    print("Starting data stream...")
     result = add.delay(100, 100)
     s = result.status
     last_s = None
@@ -25,24 +32,6 @@ def index():
         s = result.status
     
     print(s, result.result)
-
-    return render_template("index.html")
-
-# @socketio.on("start_data_stream")
-# def start_data_stream():
-
-#     print("Starting data stream...")
-#     result = add.delay(100, 100)
-#     s = result.status
-#     last_s = None
-
-#     while s != "FAILURE" or s != "SUCCESS":
-#         if s != last_s:
-#             print(s)
-#         last_s = s
-#         s = result.status
-    
-#     print(s, result.result)
 
 @cel.task()
 def add(a, b):
@@ -58,14 +47,14 @@ def add(a, b):
     # return i, value
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     if redis.Redis().ping():
-#         pass
-#     else:
-#         raise Exception("You need redis: https://redis.io/docs/getting-started/installation/. Check that redis-server.service is running!")
+    # if redis.Redis().ping():
+    #     pass
+    # else:
+    #     raise Exception("You need redis: https://redis.io/docs/getting-started/installation/. Check that redis-server.service is running!")
 
-#     ip = "192.168.1.8" # insert LAN address here
-#     port = 8080
+    ip = "192.168.1.8" # insert LAN address here
+    port = 8080
 
-#     socketio.run(app, host=ip, port=port, use_reloader=False, debug=True)
+    socketio.run(app, host=ip, port=port, use_reloader=False, debug=True)
