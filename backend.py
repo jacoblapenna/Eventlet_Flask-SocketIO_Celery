7bfcd8633384cf95dd4463e76d1875cf7bc286b3
Jacob Lapenna <jlapenna@usbr.gov>
2022-12-21 14:40:41 -0700

    Iterative commit.

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
 
-# import redis
+from random import randrange
+
+import redis
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO
@@ -21,7 +23,7 @@ def index():
 def start_data_stream():
 
     print("Starting data stream...")
-    result = add.delay(100, 100)
+    result = stream_data.delay(100, 100)
     
     s = result.state
     last_s = None
@@ -40,25 +42,25 @@ def start_data_stream():
     print(f"Task returned with status {s} and result {result.result}")
 
 @cel.task()
-def add(a, b):
-    return a + b
-    # data_socketio = SocketIO(message_queue=message_broker)
-    # i = 1
+def stream_data(a, b):
 
-    # while i <= 100:
-    #     value = randrange(0, 1000, 1) / 100
-    #     data_socketio.emit("new_data", {"value" :  value})
-    #     i += 1
+    data_socketio = SocketIO(message_queue=message_broker)
+    i = 1
+
+    while i <= 100:
+        value = randrange(0, 1000, 1) / 100
+        data_socketio.emit("new_data", {"value" :  value})
+        i += 1
     
-    # return i, value
+    return i, value
 
 
 if __name__ == "__main__":
 
-    # if redis.Redis().ping():
-    #     pass
-    # else:
-    #     raise Exception("You need redis: https://redis.io/docs/getting-started/installation/. Check that redis-server.service is running!")
+    if redis.Redis().ping():
+        pass
+    else:
+        raise Exception("You need redis: https://redis.io/docs/getting-started/installation/. Check that redis-server.service is running!")
 
     ip = "192.168.1.8" # insert LAN address here
     port = 8080
