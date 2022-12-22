fd21b14d497c9487793f9160f89c8ea2717bfbf0
Jacob Lapenna <jlapenna@usbr.gov>
2022-12-22 09:27:03 -0700

    Iterative commit.

--- backend.py
+++ backend.py
@@ -34,12 +34,12 @@ def start_data_stream():
 @cel.task()
 def stream_data(sid, message_queue):
 
-    # data_socketio = SocketIO(message_queue=message_queue)
+    data_socketio = SocketIO(message_queue=message_queue)
     i = 1
 
     while i <= 100:
         value = randrange(0, 10000, 1) / 100
-        # data_socketio.emit("new_data", {"value" :  value})
+        data_socketio.emit("new_data", {"value" :  value})
         i += 1
         time.sleep(0.01)
     
